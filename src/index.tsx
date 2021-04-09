76d4721300de52d0bc0a01863c23a6760653d0be
Diego Perozo <dperozo@qampo.es>
2021-04-09 17:21:42 -0400

    adding the frontend for the MERN APP

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Videolist from "./components/videos/Videolist";
+import Videoform from "./components/videos/Videoform";
+import Navbar from "./components/navbar/Navbar";
+import { ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Navbar />
+      <div className="container p-5">
+        <Switch>
+          <Route exact path="/" component={Videolist} />
+          <Route path="/newvideo" component={Videoform} />
+            <Route path="/update/:id" component={Videoform} />
+        </Switch>
+        <ToastContainer />
+      </div>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
